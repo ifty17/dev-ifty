a41f96f84337baa4e1fbf40a88841d085d2c8799
ifty17 <iftyrahatkabir@gmail.com>
2022-12-11 19:04:08 +0600

    added swiper

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 import pcBuilder from "../assets/pcbuilder.png";
-import natureTti from "../assets/nature.png";
+import pcBuilder2 from '../assets/pcbuilder2.png';
+import pcBuilder3 from '../assets/pcbuilder3.png';
+import nature from "../assets/nature.png";
+import nature2 from '../assets/nature2.png';
+import nature3 from '../assets/nature3.png';
 import proCoder from "../assets/procoder.png";
+import proCoder2 from '../assets/procoder2.png';
+import proCoder3 from '../assets/procoder3.png';
 
 const Portfolio = () => {
   return (
@@ -62,77 +72,39 @@ const Portfolio = () => {
           </div>
         </div>
         <div className=" my-auto mx-10 md:w-[600px] ">
-          <img
-            className="hover:scale-105 duration-500 rounded-lg"
-            src={pcBuilder}
-            alt=""
-          />
+          <Swiper
+            pagination={{
+              dynamicBullets: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img
+                className="hover:scale-105 duration-500 rounded-lg mb-8"
+                src={pcBuilder}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="hover:scale-105 duration-500 rounded-lg mb-8"
+                src={pcBuilder2}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="hover:scale-105 duration-500 rounded-lg mb-8"
+                src={pcBuilder3}
+                alt=""
+              />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
 
-      <div className="hidden md:block">
-        <div className="grid grid-cols-1 md:grid-cols-2 py-20 ">
-          <div className=" my-auto mx-10 md:w-[600px] ">
-            <img
-              className="hover:scale-105 duration-500 rounded-lg"
-              src={natureTti}
-              alt=""
-            />
-          </div>
-          <div className="mx-5 md:mx-10 md:ml-28">
-            <h1 className="text-4xl md:text-5xl">Nature tti</h1>
-            <p className="text-2xl pb-5">
-              A photography service providing Web Application.
-            </p>
-            <h4 className=" text-2xl md:text-3xl font-bold">Features:</h4>
-            <p>
-              • You can add a service to this website and you can also add a
-              review of a specific product you can also see other people's
-              reviews.
-            </p>
-            <p>
-              • When you give multiple or single reviews you can see all your
-              reviews in the My reviews option.
-            </p>
-            <p>
-              • When you login in you will see two more options in the navbar.
-            </p>
-            <p>
-              • When you login in you will see two more options in the navbar.
-            </p>
-            <div className="flex gap-3 md:gap-8">
-              <a
-                type="button"
-                target="_blank"
-                href="https://assignment-11-fd0c0.web.app/"
-                className="btn-xs md:btn-md hover:scale-110 duration-300 group cursor-pointer text-white px-3 py-3 my-2 flex items-center rounded-md bg-gradient-to-r  from-indigo-600 to-blue-500"
-                rel="noreferrer"
-              >
-                Live Site
-              </a>
-              <a
-                type="button"
-                target="_blank"
-                href="https://github.com/ifty17/Nature-tti-client"
-                className="btn-xs md:btn-md hover:scale-110 duration-300 group cursor-pointer text-white px-3 py-3 my-2 flex items-center rounded-md bg-gradient-to-r  from-indigo-600 to-blue-500"
-                rel="noreferrer"
-              >
-                Client Site
-              </a>
-              <a
-                type="button"
-                target="_blank"
-                href="https://github.com/ifty17/Nature-tti-server"
-                className="btn-xs md:btn-md hover:scale-110 duration-300 group cursor-pointer text-white px-3 py-3 my-2 flex items-center rounded-md bg-gradient-to-r  from-indigo-600 to-blue-500"
-                rel="noreferrer"
-              >
-                Server Site
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="block md:hidden">
+      <div>
         <div className="grid grid-cols-1 md:grid-cols-2 py-20 ">
           <div className="mx-5 md:mx-10 md:ml-28">
             <h1 className="text-4xl md:text-5xl">Nature tti</h1>
@@ -186,11 +158,35 @@ const Portfolio = () => {
             </div>
           </div>
           <div className=" my-auto mx-10 md:w-[600px] ">
-            <img
-              className="hover:scale-105 duration-500 rounded-lg"
-              src={natureTti}
-              alt=""
-            />
+            <Swiper
+              pagination={{
+                dynamicBullets: true,
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <img
+                  className="hover:scale-105 duration-500 rounded-lg mb-8"
+                  src={nature}
+                  alt=""
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img
+                  className="hover:scale-105 duration-500 rounded-lg mb-8"
+                  src={nature2}
+                  alt=""
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img
+                  className="hover:scale-105 duration-500 rounded-lg mb-8"
+                  src={nature3}
+                  alt=""
+                />
+              </SwiperSlide>
+            </Swiper>
           </div>
         </div>
       </div>
@@ -246,11 +242,35 @@ const Portfolio = () => {
           </div>
         </div>
         <div className=" my-auto mx-10 md:w-[600px]">
-          <img
-            className="hover:scale-105 duration-500 rounded-lg"
-            src={proCoder}
-            alt=""
-          />
+          <Swiper
+            pagination={{
+              dynamicBullets: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img
+                className="hover:scale-105 duration-500 rounded-lg mb-8"
+                src={proCoder}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="hover:scale-105 duration-500 rounded-lg mb-8"
+                src={proCoder2}
+                alt=""
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="hover:scale-105 duration-500 rounded-lg mb-8"
+                src={proCoder3}
+                alt=""
+              />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </div>
